d7f7064f0eb2be1c89f486e3dd17a6c920b901c4
Kalia Thao <kaliathao21@gmail.com>
2024-10-30 13:03:54 -0700

    Added nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,44 @@
 import { Link, useLocation } from 'react-router-dom';
 import React from 'react';
 
-function Nav() {
+// function Nav() {
+//     const currentPage = useLocation().pathname;
+
+//     return (
+//         <header>
+            // <ul className='nav nav-tabs'>
+            //     <li className='nav-item'>
+            //         <Link className={`nav-link ${currentPage === '/' ? 'active' : ''}`} to='/'>Home</Link>
+            //     </li>
+            //     <li className='nav-item'>
+            //         <Link className={`nav-link ${currentPage === '/About' ? 'active' : ''}`} to='/About'>About</Link>
+            //     </li>
+            //     <li className='nav-item'>
+            //         <Link className={`nav-link ${currentPage === '/Contact' ? 'active' : ''}`} to='/Contact'>Contact</Link>
+            //     </li>
+            // </ul>
+//         </header>
+//     );
+// }
+
+// export default Nav;
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Navigation() {
     const currentPage = useLocation().pathname;
 
-    return (
-        <header>
-            <ul className='nav nav-tabs'>
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Living Waters Church</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+          <ul className='nav nav-tabs'>
                 <li className='nav-item'>
                     <Link className={`nav-link ${currentPage === '/' ? 'active' : ''}`} to='/'>Home</Link>
                 </li>
@@ -17,8 +49,18 @@ function Nav() {
                     <Link className={`nav-link ${currentPage === '/Contact' ? 'active' : ''}`} to='/Contact'>Contact</Link>
                 </li>
             </ul>
-        </header>
-    );
+            <NavDropdown title="Ministries" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Youth</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Men
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Women</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Nav;
+export default Navigation;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,6 +8,8 @@ export default function About() {
               <div class="card-body">
                 <h2 class="card-title">About Us</h2>
                 <p>Living Waters Church (LWC) is an affiliated sister church of the greater Lao Evangelical Church. We aim to teach and spread the Gospel to people who are willing to hear the word of God. We enbrace all backgrounds and ethnicities to receive the word of God.</p>
+                <h2>Where it all Began</h2>
+                <p>First Hmong Evangelical Church is part of the Lao Evangelical Church across the U.S. Lao Evangelical Church (L.E.C.) was established in September of 1979 in Santa Ana, California. Since its establishment, L.E.C. has grown independently from one branch to nineteen branches throughout California, Nevada, Utah, Wisconsin, Minnesota, North Carolina, Georgia, and Canada.</p>
               </div>
             </div>
       </div>
